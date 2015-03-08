d5721349a655d6e66cabe229c341144c64557ba3
Nick Walker <nick@nickwalker.us>
2015-03-08 16:47:59 -0500

    Bring repo up to date

--- resume/index.php
+++ resume/index.php
@@ -1,17 +1,15 @@
 <?php
-/*
-NeueResume 1.0
-*/
+//Bootstrap - Move this code to wherever you want the resume to display on your server
+// This is the barest implementation to get the package off the ground
 
-require('neueresume/neueresume.php');
+require 'vendor/autoload.php'; //Don't try to make these paths absolute (begin with a /) unless you know what you're doing.
 
-$neuegal = new NeueResume;
+$resumePathFromRoot = realpath('resume.xml');
+									
+$themePathFromRoot = realpath('themes/default'); //Where is your theme?
+												 //Note that the theme MUST be in a publicly accesible directory!
+												 //Otherwise your CSS won't load :(
 
-$neuegal->vars['version'] = '0.1';
+$neueresume = new \Nickswalker\NeueResume\NeueResume($resumePathFromRoot, $themePathFromRoot);
 
-$neuegal->startTimer();
-
-$neuegal->loadSettings();
-$neuegal->initialize();
-
-?>
+$neueresume->display();
